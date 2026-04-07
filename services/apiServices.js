28b9249c8c26542d8c30fd61d2200f667e041b41
Erick Fabian <erick.fabian.gtz@gmail.com>
2026-04-07 11:08:08 -0600

    refactor(frontend): cambio a rutas relativas para proxy inverso con Nginx

--- services/apiServices.js
+++ services/apiServices.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = '/api';
 
 export async function obtenerProductos() {
     const response = await fetch(`${API_BASE_URL}/productos`);
